d700be36f36503cecb70e760bfe822ac310a767b
Porco <porcorossoj89@gmail.com>
2024-03-06 03:06:13 +0400

    rm proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,14 +4,6 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), nodePolyfills()],
-	server: {
-		proxy: {
-			"/": {
-				target: "https://aztec-pxe.abstract-crypto.com",
-				changeOrigin: true,
-			},
-		},
-	},
 	optimizeDeps: {
 		esbuildOptions: {
 			target: "esnext",
